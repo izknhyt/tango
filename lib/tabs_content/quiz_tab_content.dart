c716a9147a9865720f8aeae038016d4a41df8357
izknhyt <nemutesu@gmail.com>
2025-06-22 15:05:37 +0900

    Add review mode dropdown and propagate mode

--- lib/tabs_content/quiz_tab_content.dart
+++ lib/tabs_content/quiz_tab_content.dart
@@ -2,16 +2,19 @@
 import 'package:flutter/material.dart';
 import '../app_view.dart'; // AppScreenとScreenArgumentsのため (もしnavigateToで使うなら)
 import '../quiz_setup_screen.dart';
+import '../review_service.dart';
 
 class QuizTabContent extends StatelessWidget {
   // navigateToコールバックを受け取るためのfinal変数を追加
   final Function(AppScreen screen, {ScreenArguments? args})? navigateTo;
+  final ReviewMode mode;
 
   // コンストラクタでnavigateToを受け取るように修正 (任意パラメータとして)
-  const QuizTabContent({Key? key, this.navigateTo}) : super(key: key);
+  const QuizTabContent({Key? key, this.navigateTo, required this.mode})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const QuizSetupScreen();
+    return QuizSetupScreen(mode: mode);
   }
 }
