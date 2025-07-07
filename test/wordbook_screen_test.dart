1fce1dba86e8a39d5bd5bfce3cbdb1804c73f23a
izknhyt <nemutesu@gmail.com>
2025-07-07 13:31:39 +0900

    feat: add overlay controls to wordbook

--- test/wordbook_screen_test.dart
+++ test/wordbook_screen_test.dart
@@ -107,4 +107,51 @@ void main() {
     expect(find.byIcon(Icons.chevron_left), findsOneWidget);
     expect(find.byIcon(Icons.chevron_right), findsOneWidget);
   });
+
+  testWidgets('tap toggles page controls', (tester) async {
+    SharedPreferences.setMockInitialValues({'bookmark_pageIndex': 0});
+    final prefs = await SharedPreferences.getInstance();
+    await tester.pumpWidget(MaterialApp(
+      home: WordbookScreen(
+        flashcards: cards,
+        prefsProvider: () async => prefs,
+      ),
+    ));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Slider), findsNothing);
+
+    await tester.tap(find.byType(PageView));
+    await tester.pumpAndSettle();
+    expect(find.byType(Slider), findsOneWidget);
+
+    await tester.tap(find.byType(PageView));
+    await tester.pumpAndSettle();
+    expect(find.byType(Slider), findsNothing);
+  });
+
+  testWidgets('slider changes page and saves bookmark', (tester) async {
+    SharedPreferences.setMockInitialValues({'bookmark_pageIndex': 0});
+    final prefs = await SharedPreferences.getInstance();
+    await tester.pumpWidget(MaterialApp(
+      home: WordbookScreen(
+        flashcards: cards,
+        prefsProvider: () async => prefs,
+      ),
+    ));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.byType(PageView));
+    await tester.pumpAndSettle();
+
+    final sliderFinder = find.byType(Slider);
+    final start = tester.getTopLeft(sliderFinder);
+    final end = tester.getTopRight(sliderFinder);
+    final y = (start.dy + end.dy) / 2;
+    await tester.tapAt(Offset(end.dx - 1, y));
+    await tester.pumpAndSettle();
+
+    expect(find.text('(2 / 2)'), findsOneWidget);
+    expect(prefs.getInt('bookmark_pageIndex'), 1);
+  });
 }
