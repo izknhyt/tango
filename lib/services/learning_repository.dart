db541ea757649f6902b647f70e64f22664209256
izknhyt <nemutesu@gmail.com>
2025-07-19 07:59:50 +0900

    fix: return LearningRepository instance

--- lib/services/learning_repository.dart
+++ lib/services/learning_repository.dart
@@ -10,17 +10,18 @@ class LearningRepository {
 
   LearningRepository._(this._box);
 
-  /// Open the Hive box used for stats.
-  static Future<Box<LearningStat>> open() async {
+  /// Open the Hive box used for stats and return a repository.
+  static Future<LearningRepository> open() async {
+    Box<LearningStat> box;
     if (Hive.isBoxOpen(boxName)) {
-      return Hive.box<LearningStat>(boxName);
+      box = Hive.box<LearningStat>(boxName);
+    } else {
+      if (!Hive.isAdapterRegistered(LearningStatAdapter().typeId)) {
+        Hive.registerAdapter(LearningStatAdapter());
+      }
+      box = await Hive.openBox<LearningStat>(boxName);
     }
-
-    if (!Hive.isAdapterRegistered(LearningStatAdapter().typeId)) {
-      Hive.registerAdapter(LearningStatAdapter());
-    }
-
-    return Hive.openBox<LearningStat>(boxName);
+    return LearningRepository._(box);
   }
 
   LearningStat get(String wordId) {
